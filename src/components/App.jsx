b1247fc85788c59cc85aa14ec3c998a32255ba9e
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-08-26 22:38:33 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from "./AuthInputs";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
