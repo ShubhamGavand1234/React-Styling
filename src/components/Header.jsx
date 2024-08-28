ee8f327c76720cd9838ad83a36bf7dfea74b1b1f
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-08-28 20:05:24 +0530

    Styled Component using react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "../assets/logo.png";
+import clasess from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={clasess.paragraph}>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
